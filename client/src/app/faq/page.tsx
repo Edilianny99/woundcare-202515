6b5b3e6b31f7be4c4fb88ed386be77ada3ed0f6e
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2024-11-17 17:10:42 -0400

    Estilos de nuevo FAQ

--- client/src/app/faq/page.tsx
+++ client/src/app/faq/page.tsx
@@ -1,47 +1,81 @@
-"use client";
-import {
-	Accordion,
-	AccordionButton,
-	AccordionIcon,
-	AccordionItem,
-	AccordionPanel,
-	Box,
-	Heading,
-} from "@chakra-ui/react";
 import { faq } from "./questions";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
+import React from "react";
+import Arrow from "@/components/Arrow";
 
 function myPage() {
 	return (
-		<Box as="main" width="100vw" minHeight="100vh" padding={5}>
-			<Box
-				as="article"
-				fontSize={15}
-				display="flex"
-				flexDirection="column"
-				gap={6}
-			>
-				<Heading as="h1" fontSize={"1.25rem"}>
-					Preguntas frecuentes
-				</Heading>
-				<Accordion allowMultiple>
-					{faq.map((item, index) => (
-						<AccordionItem key={index} borderColor={"purple"}>
-							<h2>
+		<>
+			<Box as="main" flex={1}>
+				<Arrow />
+				<Flex
+					marginTop={-16}
+					marginRight={6}
+					flexDirection="column"
+					alignItems="flex-end"
+				>
+					<Heading
+						as="h1"
+						color="#4F1964"
+						borderBottom={"2px solid #AD8EB1"}
+						paddingX="10px"
+					>
+						Preguntas Frecuentes
+					</Heading>
+				</Flex>
+				<Flex
+					w="100vw"
+					pl="20px"
+					pr="20px"
+					mb="30px"
+					mt="30px"
+					direction="column"
+					align="center"
+				>
+					<Accordion allowToggle>
+						{faq.map((item, index) => (
+							<AccordionItem
+								key={index}
+								bg="#AD8EB1"
+								p="10px"
+								borderRadius="10px"
+								w="90vw"
+								color="white"
+								fontWeight="bold"
+								boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+								mb="20px"
+							>
 								<AccordionButton>
-									<Box as="span" flex="1" textAlign="left" fontSize={"1rem"}>
+									<Box flex="1" textAlign="left">
 										{item.question}
 									</Box>
 									<AccordionIcon />
 								</AccordionButton>
-							</h2>
-							<AccordionPanel pb={4} fontSize={"0.75rem"}>
-								{item.answer}
-							</AccordionPanel>
-						</AccordionItem>
-					))}
-				</Accordion>
+								<AccordionPanel
+									pb={4}
+									bg="white"
+									color="#3B3B3B"
+									fontWeight="400"
+									borderRadius="10px"
+								>
+									{item.answer}
+								</AccordionPanel>
+							</AccordionItem>
+						))}
+					</Accordion>
+				</Flex>
 			</Box>
-		</Box>
+		</>
 	);
 }
 
